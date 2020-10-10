ada5576a200fd272da45839529117b7f30b55d81
Dhruv Bhanushali <dhruv_b@live.com>
2020-10-11 03:29:18 +0530

    Update the API to use TypeScript and a new URL scheme

---
+++ api/blog_posts/[slug].ts
@@ -0,0 +1,56 @@
+import { NowRequest, NowResponse } from '@vercel/node'
+
+import frontMatter from 'gray-matter'
+import MarkdownIt from 'markdown-it'
+
+import { repository, client } from '../_utils/github'
+import { insertDates, insertUrls } from './_utils/posts'
+import { Retrieve, Post as Attributes } from './_utils/types'
+
+const markdownIt = new MarkdownIt({ html: true })
+
+const logic = async (slug: string, res: NowResponse): Promise<void> => {
+  const payload = {
+    query: `
+      query($repoOwner: String!, $repoName: String!, $objExpression: String) {
+        repository(owner: $repoOwner, name: $repoName) {
+          file: object(expression: $objExpression) {
+            ...on Blob {
+              text
+            }
+          }
+        }
+      }
+    `,
+    variables: {
+      repoOwner: repository.owner,
+      repoName: repository.name,
+      objExpression: `master:posts/${slug}.md`
+    }
+  }
+
+  const { repository: { file: { text } } }: Retrieve = await client(payload.query, payload.variables)
+
+  let { data: attributes, content: body } = frontMatter(text)
+
+  insertDates(attributes as Attributes)
+  insertUrls(attributes as Attributes)
+
+  body = markdownIt.render(body)
+
+  res.status(200).json({
+    attributes,
+    body
+  })
+}
+
+export default async (req: NowRequest, res: NowResponse): Promise<void> => {
+  let { slug: slugQuery = 'hello_world' } = req.query
+  if (Array.isArray(slugQuery)) {
+    slugQuery = slugQuery[0]
+  }
+
+  const slug = slugQuery
+
+  await logic(slug, res)
+}
---
+++ api/blog_posts/index.ts
@@ -0,0 +1,68 @@
+import { NowRequest, NowResponse } from '@vercel/node'
+
+import { repository, client } from '../_utils/github'
+import { insertDates, insertUrls } from './_utils/posts'
+import { Entry, List, Post } from './_utils/types'
+
+const logic = async (offset: number, count: number, res: NowResponse): Promise<void> => {
+  const payload = {
+    query: `
+      query($repoOwner: String!, $repoName: String!, $objExpression: String) {
+        repository(owner: $repoOwner, name: $repoName) {
+          tree: object(expression: $objExpression) {
+            ...on Tree {
+              entries {
+                name
+                file: object {
+                  ...on Blob {
+                    text
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    `,
+    variables: {
+      repoOwner: repository.owner,
+      repoName: repository.name,
+      objExpression: `metadata:metadata`
+    }
+  }
+
+  const { repository: { tree: { entries } } }: List = await client(payload.query, payload.variables)
+
+  const totalCount = entries.length - 1 // Hide blog post #0
+  const posts = (entries)
+    .sort((a: Entry, b: Entry): number => -a.name.localeCompare(b.name))
+    .slice(offset, Math.min(offset + count, totalCount))
+    .map(entry => {
+      let post: Post = JSON.parse(entry.file.text)
+
+      insertDates(post)
+      insertUrls(post)
+
+      return post
+    })
+
+  res.status(200).json({
+    totalCount,
+    posts
+  })
+}
+
+export default async (req: NowRequest, res: NowResponse): Promise<void> => {
+  let { offsetQuery = '0', countQuery = '5' } = req.query
+  if (Array.isArray(offsetQuery)) {
+    offsetQuery = offsetQuery[0]
+  }
+  if (Array.isArray(countQuery)) {
+    countQuery = countQuery[0]
+  }
+
+  const offsetNum = parseInt(offsetQuery)
+  const countNum = parseInt(countQuery)
+
+  await logic(offsetNum, countNum, res)
+}
